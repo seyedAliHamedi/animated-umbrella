4b46d6decf758444e5c0f219d1e14db1d635ac19
hamediseyedali <hamediseyedali@gmail.com>
2025-02-11 16:48:23 +0330

    simple network topology

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ cd /Users/alihamedi/Desktop/work/ns3/ns-3-allinone/ns-3-dev/ || exit 1
 
 # Start ns3 shell and run the Python script
 ./ns3 shell <<EOF
-python ./sdn/main.py
+python ./animated-umbrella/main.py
 exit
 EOF
