58c576eab2f67fc1533f774bfe37a11fb1429930
hamediseyedali <hamediseyedali@gmail.com>
2025-02-11 17:28:34 +0330

    3subnets architecture

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ cd /Users/alihamedi/Desktop/work/ns3/ns-3-allinone/ns-3-dev/ || exit 1
 
 # Start ns3 shell and run the Python script
 ./ns3 shell <<EOF
-python ./animated-umbrella/main.py
+python ./animated-umbrella/main3.py
 exit
 EOF
